b60b39e297bef4b701f022289318ee51ada62a51
Mike <96078399+Kontosoros@users.noreply.github.com>
2025-09-01 16:34:19 +0300

    add hearing button

--- frontend/bark-app-ui/src/app/components/recorder-area/recorder-area.component.ts
+++ frontend/bark-app-ui/src/app/components/recorder-area/recorder-area.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-recorder-area',
-  imports: [],
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './recorder-area.component.html',
-  styleUrl: './recorder-area.component.scss'
+  styleUrl: './recorder-area.component.scss',
 })
 export class RecorderAreaComponent {
+  isRecording = false;
 
+  toggleRecording() {
+    this.isRecording = !this.isRecording;
+    if (this.isRecording) {
+      this.startRecording();
+    } else {
+      this.stopRecording();
+    }
+  }
+
+  startRecording() {
+    console.log('Recording started');
+    // Add your recording logic here
+  }
+
+  stopRecording() {
+    console.log('Recording stopped');
+    // Add your stop recording logic here
+  }
 }
